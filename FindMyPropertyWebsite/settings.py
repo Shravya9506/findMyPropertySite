5ee05c6d14104ae6fc30ad1ef3ed58df0cca8a91
ShravyaChandiri <schandiri@unomaha.edu>
2021-02-28 19:18:52 -0600

    Update settings.py

--- FindMyPropertyWebsite/settings.py
+++ FindMyPropertyWebsite/settings.py
@@ -76,22 +76,22 @@ WSGI_APPLICATION = 'FindMyPropertyWebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'efs',
-#         'USER': 'postgres',
-#         'PASSWORD': 'admin',
-#         'PORT': '5433'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'efs',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'PORT': '5433'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
@@ -150,4 +150,4 @@ EMAIL_USE_TLS = True
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL='/'
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
