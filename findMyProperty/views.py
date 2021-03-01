c53c6446be95c57bbe9c35c7b7578afc97ee7723
ShravyaChandiri <schandiri@unomaha.edu>
2021-02-28 19:21:22 -0600

    Update views.py

--- findMyProperty/views.py
+++ findMyProperty/views.py
@@ -10,11 +10,12 @@ def home(request):
         return render(request, 'home.html', {'properties' : properties})
     elif Seller.objects.filter(user_id=request.user.id).values_list('user_id', flat=True).distinct().count() > 0:
         return redirect('properties:view_your_properties', request.user.id)
-    else:
+    elif Buyer.objects.filter(user_id=request.user.id).values_list('user_id', flat=True).distinct().count() > 0:
         buyer = get_object_or_404(Buyer, user_id=request.user.id)
         favs = CustomerFavoriteProperty.objects.filter(buyer=buyer).values_list('property_id', flat=True).distinct()
         print(list(favs))
         return render(request, 'look_up_property.html', {'properties': properties, 'favorites': list(favs)})
-
+    else:
+        return render(request, 'home.html', {'properties': properties})
 def about_us(request):
     return render(request, 'about_us.html')
